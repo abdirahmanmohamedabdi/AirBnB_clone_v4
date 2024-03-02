fe738d1f553c426520601d3b933dedc126bea670
mumvekeri <mumvekerichidochashe@gmail.com>
2024-03-02 12:25:49 -0800

    try

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+  const amenityIds = {};
+
+  $('input[type=checkbox]').change(function () {
+    if ($(this).prop('checked')) {
+      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityIds[$(this).attr('data-id')];
+    }
+
+    if (Object.keys(amenityIds).length === 0) {
+      $('div.amenities h4').html('&nbsp;');
+    } else {
+      $('div.amenities h4').text(Object.values(amenityIds).join(', '));
+    }
+  });
+
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    type: 'GET',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    },
+    error: function () {
+      console.error('Error fetching API status.');
+    }
+  });
+
+  // Request places search
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    dataType: 'json',
+    success: function (data) {
+      data.forEach(function (place) {
+        $('section.places').append(
+          `<article>
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">$${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+              <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+              <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+            </div>
+            <div class="description">${place.description}</div>
+          </article>`
+        );
+      });
+    },
+    error: function () {
+      console.error('Error fetching places data.');
+    }
+  });
+});
+
